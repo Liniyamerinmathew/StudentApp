dfaca1debd32053168fd4786a628d5f0e319a4c2
Liniya Merin Mathew <Liniya@NTP-LAP-788.chn.nestgroup.net>
2022-11-22 06:38:10 +0530

    menu

---
+++ src/studentApp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+    public class studentApp {
+        static Scanner sc = new Scanner(System.in);
+        public static void main(String[] args) {
+            while(true) {
+                System.out.println("1. Add Student");
+                System.out.println("2. View Students");
+                System.out.println("3. Search Student");
+                System.out.println("4. Delete Student");
+                System.out.println("5. Exit");
+                System.out.print("\nPlease enter the choice: ");
+                int choice = sc.nextInt();
+
+            }
+        }
+    }
+}
+}
